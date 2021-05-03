aaf1756830414fa76e8d69784be3d78aa2def89e
yabby <yabby1997@gmail.com>
2021-05-03 21:54:49 +0900

    TrafficLight 기능 구현

--- UIKitCollaborationTest/ViewController.swift
+++ UIKitCollaborationTest/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TrafficLight {
+    case green
+    case yellow
+    case red
+    
+    mutating func next() {
+        switch(self) {
+        case .green:
+            self = .yellow
+        case .yellow:
+            self = .red
+        case .red:
+            self = .green
+        }
+    }
+}
 
+class ViewController: UIViewController {
+    // MARK: - Properties
+    
+    var state: TrafficLight = .green {
+        didSet {
+            switch(state) {
+            case .green:
+                self.view.backgroundColor = .green
+            case .yellow:
+                self.view.backgroundColor = .yellow
+            case .red:
+                self.view.backgroundColor = .red
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
 
-
+    // MARK: - IBAction
+    
+    @IBAction func didTapTrafficLight(_ sender: Any) {
+        self.state.next()
+    }
 }
 
